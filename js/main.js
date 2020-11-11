e73250d89da6ab4afeb4b0ffbcad7c890706feea
ivanburda1986 <burda.ivan@gmail.com>
2020-11-12 00:47:37 +0100

    visualized the days

--- js/main.js
+++ js/main.js
@@ -12,10 +12,20 @@ function Day(){
 }
 
 // LOGICS
-function createDays(expiration, dob){
-  let numberOfDays = (dob-expiration)/1000/3600/24/365;
-  for(let i=0; i < numberOfDays; i++){
-
-    //data.days.push(new Day({year: , month: , day: }));
+function createDaysDataObjects(expiration, dob){
+  let numberOfDays = (expiration-dob)/1000/3600/24;
+  let nextDate = dob;
+  for(let i = 1; i < numberOfDays; i++){
+    nextDate.setDate(nextDate.getDate() + 1);
+    data.days.push(new Date(nextDate));
   }
+};
+
+function visualizeDays(days){
+  days.forEach(day => {
+    let domDay = document.createElement("div");
+    domDay.classList.add('day');
+    domDay.id = `${day.getYear()}-${day.getMonth()}-${day.getDate()}`;
+    document.getElementById('mainVisualization').appendChild(domDay);
+  })
 }
