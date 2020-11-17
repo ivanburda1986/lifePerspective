8dc59759287f16bf55493e698f6a3cb32f8f8984
ivanburda1986 <burda.ivan@gmail.com>
2020-11-18 00:30:43 +0100

    working on the country selector question

--- js/index.js
+++ js/index.js
@@ -1,16 +1,19 @@
 const ui = {
   nextBtn: document.getElementById('nextBtn'),
+  questionCountry: document.getElementById('question-country'),
+  questionGender: document.getElementById('question-gender')
 };
 
 
 //Make the 'Next' button invisible by default
-ui.nextBtn.visibility = 'hidden';
+ui.nextBtn.style.display = 'none';
+ui.questionCountry.style.display = 'none';
 
 
 //Display the next button
 Array.from(document.querySelectorAll('.radioOption')).forEach(radioOption =>{
   radioOption.addEventListener('click', (e)=>{
-    ui.nextBtn.visibility = 1;
+    ui.nextBtn.style.display = 'flex';
   })
 });
 
@@ -19,4 +22,7 @@ Array.from(document.querySelectorAll('.radioOption')).forEach(radioOption =>{
 ui.nextBtn.addEventListener('click', (e)=>{
   e.preventDefault();
   localStorage.setItem('gender', document.querySelector('input[name="gender"]:checked').value);
+  ui.nextBtn.style.display = 'none';
+ui.questionGender.style.display = 'none';
+ui.questionCountry.style.display = 'flex';
 })
