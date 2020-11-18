a4d96868b0ec4ab3651bb84379d4d3395ff16d56
ivanburda1986 <burda.ivan@gmail.com>
2020-11-18 23:57:16 +0100

    working on the list of countries

--- js/index.js
+++ js/index.js
@@ -6,14 +6,14 @@ const ui = {
 
 
 //Make the 'Next' button invisible by default
-ui.nextBtn.style.display = 'none';
+ui.nextBtn.style.visibility = 'hidden';
 ui.questionCountry.style.display = 'none';
 
 
 //Display the next button
 Array.from(document.querySelectorAll('.radioOption')).forEach(radioOption =>{
   radioOption.addEventListener('click', (e)=>{
-    ui.nextBtn.style.display = 'flex';
+    ui.nextBtn.style.visibility = 'visible';
   })
 });
 
@@ -22,7 +22,18 @@ Array.from(document.querySelectorAll('.radioOption')).forEach(radioOption =>{
 ui.nextBtn.addEventListener('click', (e)=>{
   e.preventDefault();
   localStorage.setItem('gender', document.querySelector('input[name="gender"]:checked').value);
-  ui.nextBtn.style.display = 'none';
+ui.nextBtn.style.visibility = 'hidden';
 ui.questionGender.style.display = 'none';
 ui.questionCountry.style.display = 'flex';
 })
+
+//Get the countries and related life expectancy data
+async function getData() {
+  const res = await fetch(
+    '../data/life-expectancy.json'
+  );
+  const data = await res.json();
+  return data;
+}
+
+let countries = getData();
