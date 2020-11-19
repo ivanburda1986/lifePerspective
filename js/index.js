cb445ebebb342e8f9217951fd9e2ca71c77637d6
ivanburda1986 <burda.ivan@gmail.com>
2020-11-19 01:29:13 +0100

    finished the country selector

--- js/index.js
+++ js/index.js
@@ -1,13 +1,20 @@
 const ui = {
   nextBtn: document.getElementById('nextBtn'),
   questionCountry: document.getElementById('question-country'),
-  questionGender: document.getElementById('question-gender')
+  questionGender: document.getElementById('question-gender'),
+  questionDob: document.getElementById('question-dob'),
+  countryselect: document.getElementById('countryselect'),
 };
+
+const data = {
+  currentQuestion: 1
+}
 
 
 //Make the 'Next' button invisible by default
 ui.nextBtn.style.visibility = 'hidden';
 ui.questionCountry.style.display = 'none';
+ui.questionDob.style.display = 'none';
 
 
 //Display the next button
@@ -21,10 +28,22 @@ Array.from(document.querySelectorAll('.radioOption')).forEach(radioOption =>{
 //Save the value
 ui.nextBtn.addEventListener('click', (e)=>{
   e.preventDefault();
-  localStorage.setItem('gender', document.querySelector('input[name="gender"]:checked').value);
-ui.nextBtn.style.visibility = 'hidden';
-ui.questionGender.style.display = 'none';
-ui.questionCountry.style.display = 'flex';
+  if(data.currentQuestion === 1){
+    localStorage.setItem('gender', document.querySelector('input[name="gender"]:checked').value);
+    ui.questionGender.style.display = 'none';
+    ui.questionDob.style.display = 'flex';
+    data.currentQuestion = 2;
+  }
+  else if(data.currentQuestion === 2){
+    ui.questionDob.style.display = 'none';
+    ui.questionCountry.style.display = 'flex';
+    data.currentQuestion = 3;
+    showData();
+  }
+  else if(data.currentQuestion === 3){
+    localStorage.setItem('country', ui.countryselect.value);
+    window.open("main.html", "_self");
+  }
 })
 
 //Get the countries and related life expectancy data
@@ -36,4 +55,19 @@ async function getData() {
   return data;
 }
 
-let countries = getData();
+//Create the selection of countries
+async function showData(){
+  let data = await getData();
+  let countryList = [];
+  data.map(dataItem => {
+    countryList.push(dataItem.country);
+  });
+  countryList.sort();
+  let htmlCountryList = "";
+  countryList.forEach(function (country) {
+    htmlCountryList += `
+    <option value="${country}">${country}</option>
+    `;
+  });
+  countryselect.innerHTML = htmlCountryList;
+};
